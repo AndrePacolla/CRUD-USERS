8400c8bd6b4ad7f216c374b1caefe1137590f2ae
AndrePacolla <andreluizzpacolla@outlook.com>
2024-09-22 19:13:59 -0300

    adc folder components and styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Global from "./styles/global";
+import styled from "styled-components";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Form from "./components/Form";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const Container = styled.div`
+
+    width: 100%;
+    max-width: 800px;
+    margin-top: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+  `
+
+ const Title = styled.h2`
+    color: white;
+    border-bottom: 2px solid #6e6d6d;
+ `;
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <Container>
+
+    <Title>USUÁRIOS</Title>
+
+    <Form></Form>
+   
+    </Container>
+
+
+    <Global/>
+    <ToastContainer autoClose={3000} position="bottom-right"/>
     </>
   )
 }
